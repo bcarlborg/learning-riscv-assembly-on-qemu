98e42aa7406f188d71cf75ccc5a5016c9202c1ac
Beau Carlborg <bcarlborg@gmail.com>
2024-04-15 21:29:56 -0700

    simply jump to entry in a tight loop

--- 00-setting-up-a-build-and-run-environment/entry.s
+++ 00-setting-up-a-build-and-run-environment/entry.s
@@ -6,18 +6,4 @@
     .global _entry
 
 _entry:
-    # Load immediate values into registers a0 and a1
-    li a0, 10    # Load the first number (10) into register a0
-    li a1, 20    # Load the second number (20) into register a1
-
-    # Add the numbers in a0 and a1; store the result in a1
-    add a1, a0, a1
-
-    # Normally, you would have an exit syscall here for a standalone program
-    # For simplicity, this example ends without a system call to exit
-
-.text
-    # other programs can go here
-
-# TODO: get end symbol into assembly somehow. Is that how we get the stack?
-# TODO: does qemu put the stack here?
+    j _entry
